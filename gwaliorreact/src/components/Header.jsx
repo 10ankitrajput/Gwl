1eef89478a6b707222a9d0eb8930668863efa951
10ankitrajput <10ankitsinghrajput@gmail.com>
2023-05-01 14:08:50 +0530

    okk

--- gwaliorreact/src/components/Header.jsx
+++ gwaliorreact/src/components/Header.jsx
@@ -1,23 +1,31 @@
+import { useState } from "react";
 import "./Header.css";
+
+
 
 function Header() {
 
-let myfunc = () =>{
-  let mydiv = document.getElementById("nav");
-  if(mydiv.className === "head-div"){
-    mydiv.className += " head-div-res";
-    alert("Hello")
-  }else{
-    mydiv.className = "head-div"
-  }
+  const [Menu,setMenu ] = useState("Header-res");
+  const [Li,setLi] = useState("display-hide")
   
+let myfunc = () =>{
+    if(Menu === "Header-res")
+    {
+      setMenu("");
+      setLi("")
+    }
+    else{
+      setMenu("Header-res")
+      setLi("display-show")
+    }
+
 }
 
   return (
     <div className="head-div" id="nav">
       <div className="Header">
         <div>LOGO</div>
-        <ul>
+        <ul className={Li}>
           <li>
             <a href="/" className="item1">
               Home
@@ -49,6 +57,37 @@ let myfunc = () =>{
           <a href="javascript:void(0)" className="icon" onClick={myfunc}><i class="fa-sharp fa-solid fa-bars"></i></a>
           </li>
         </ul>
+      </div>
+      <div className={Menu}>
+      <ul>
+          <li>
+            <a href="/" className="item1">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="/" className="item2">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="/" className="item4">
+              Project
+            </a>
+          </li>
+
+          <li>
+            <a href="/" className="item4">
+              Transportation
+            </a>
+          </li>
+          <li>
+            <a href="/" className="item3">
+              ContactUs
+            </a>
+          </li>
+          </ul>
       </div>
     </div>
   );
