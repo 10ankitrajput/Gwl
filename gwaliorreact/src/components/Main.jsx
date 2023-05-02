2f2ba45674da9f68bc992586f0bdea3bf317de09
10ankitrajput <10ankitsinghrajput@gmail.com>
2023-05-02 21:53:13 +0530

    22:00

--- gwaliorreact/src/components/Main.jsx
+++ gwaliorreact/src/components/Main.jsx
@@ -8,7 +8,7 @@ const Main=()=>{
 
     return(
         <div className="Main-div">
-            <h1>Gwaliorites</h1>
+            <h2>Gwaliorites</h2>
             <h2>We Are Coming Soon</h2>
         </div>
     )
